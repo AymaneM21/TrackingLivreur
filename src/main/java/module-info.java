5b80c8ca2c1fcaf0828b3276d968c1487ecfe2c3
Aymane <aymane12maf@gmail.com>
2023-04-01 17:05:56 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.trackinglivreur {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.trackinglivreur to javafx.fxml;
+    exports com.example.trackinglivreur;
+}
